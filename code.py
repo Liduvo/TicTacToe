7721e57607a18db911d653788d0dab695fc41869
Utku Hasbey Kurban <69817471+Liduvo@users.noreply.github.com>
2022-01-15 09:08:46 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,99 +1,58 @@
-class Game():
+board = ["-","-","-","-","-","-","-","-","-"]
 
-    def __init__(self):
-        self.game_board = [["[ ]","[ ]","[ ]"],["[ ]","[ ]","[ ]"],["[ ]","[ ]","[ ]"]]
-        self.status = True
-        self.move = 0
+def dis():
+    print("|"+ board[0] + "|" + board[1] + "|" + board[2])
+    print("=========")
+    print("|" + board[3] + "|" + board[4] + "|" + board[5])
+    print("=========")
+    print("|" + board[6] + "|" + board[7] + "|" + board[8])
 
-    def play(self):
-        if self.move % 2 == 0:
-            self.p1chose()
-        else:
-            self.p2chose()
-
-        self.status = self.game_control()
-
-        self.move += 1
-
-    def p1chose(self):
-        self.game_board_wiev()
-        print("Player 1,")
-
-        line = int(input("Enter a line: "))
-        while line < 1 or line > 3:
-            line = int(input("The number value to be entered can be 1 ,2 and 3."))
-
-        column = int(input("Enter a column: "))
-        while line < 1 or line > 3:
-            line = int(input("The number value to be entered can be 1 ,2 and 3."))
-
-        if self.is_it_full(line,column):
-            self.p1chose()
-        else:
-            self.game_board[line][column] = "X"
-
-    def p2chose(self):
-        self.game_board_wiev()
-        print("Player 1,")
-
-        line = int(input("Enter a line: "))
-        while line < 1 or line > 3:
-            line = int(input("The number value to be entered can be 1 ,2 and 3."))
-
-        column = int(input("Enter a column: "))
-        while line < 1 or line > 3:
-            line = int(input("The number value to be entered can be 1 ,2 and 3."))
-
-        if self.is_it_full(line,column):
-            self.p2chose()
-        else:
-            self.game_board[line][column] = "0"
-
-    def is_it_full(self,line,column):
-        if self.game_board[line][column] != "":
-            return True
-        else:
-            return False
-
-    def game_board_wiev(self):
-        for i in self.tahta:
-            for j in i:
-                print(j,end=" ")
-
-            print("\n")
-
-    def game_control(self):
-        if [self.game_board[0][0], self.game_board[0][1], self.game_board[0][2]] == ["X","X","X"] or [self.game_board[0][0], self.game_board[0][1],self,game_board[0][2]] == ["O","O","O"]:
-            return False
-        
-        if [self.game_board[1][0], self.game_board[1][1], self.game_board[1][2]] == ["X","X","X"] or [self.game_board[1][0], self.game_board[1][1], self.game_board[1][2]] == ["O","0","0"]:
-            return False
-            
-        if [self.game_board[2][0], self.game_board[2][1], self.game_board[2][2]] == ["X","X","X"] or [self.game_board[2][0], self.game_board[2][1], self.game_board[2][2]] == ["O","O","O"]:
-            return False
-
-
-        if [self.game_board[0][0], self.game_board[0][1], self.game_board[0][2]] == ["X","X","X"] or [self.game_board[0][0], self.game_board[0][1], self.game_board[0][2]] == ["O","O","O"]:
-            return False
-
-        if [self.game_board[1][0], self.game_board[1][1], self.game_board[1][2]] == ["X","X","X"] or [self.game_board[1][0], self.game_board[1][1], self.game_board[1][2]] == ["O","O","O"]:
-            return False
-
-        if [self.game_board[2][0], self.game_board[2][1], self.game_board[2][2]] == ["X","X","X"] or [self.game_board[2][0], self.game_board[2][1], self.game_board[2][2]] == ["O","O","O"]:
-            return False
-
-        
-        if [self.game_board[0][0], self.game_board[1][1], self.game_board[2][2]] == ["X","X","X"] or [self.game_board[0][0], self.game_board[0][1], self.game_board[0][2]] == ["O","O","O"]:
-            return False
-
-        if [self.game_board[0][2], self.game_board[1][1], self.game_board[2][0]] == ["X","X","X"] or [self.game_board[0][0], self.game_board[0][1], self.game_board[0][2]] == ["O","O","O"]:
-            return False
-
+def check(board):
+    p1 = "X"
+    p2 = "O"
+    if board[0] == board [1] == board[2] == p1 or board[0] == board [1] == board[2] == p2:
         return True
+    elif board[3] == board [4] == board[5] == p1 or board[3] == board [4] == board[5] == p2:
+        return True
+    elif board[6] == board [7] == board[8] == p1 or board[6] == board [7] == board[8] == p2:
+        return True
+    elif board[1] == board [4] == board[7] == p1 or board[1] == board [4] == board[7] == p2:
+        return True
+    elif board[2] == board [5] == board[8] == p1 or board[3] == board [4] == board[5] == p2:
+        return True
+    elif board[0] == board [4] == board[8] == p1 or board[0] == board [4] == board[8] == p2:
+        return True
+    elif board[2] == board [4] == board[6] == p1 or board[2] == board [4] == board[6] == p2:
+        return True
+    else:
+        return False
 
-        
+def inp(board):
+    x = int(input("Please enter the position: "))
 
-game = Game()
+    if board[x-1] != "-":
+        print("Choose another place this place is full")
+        return inp(board)
 
-while game.status():
-    Game.play()
+    else:
+        return x
+player1 = input("Player 1 nane: ")
+player2 = input("Player 2 name: ")
+dis()
+for i in range(9):
+    if i%2 == 0:
+        x = inp(board)
+        board[x-1] = "X"
+        dis()
+        if check(board):
+            print("Player 1 win")
+            break
+    else:
+        x = inp(board)
+        board[x - 1] = "O"
+        dis()
+        if check(board):
+            print("Player 2 win")
+            break
+
+    print("Finish")
